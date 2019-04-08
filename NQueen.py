8ff4d85da12b9fdb35a02601afa9f7ecaf84fb75
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-04-08 14:58:59 -0300

    Create function that calculates fitness of solution

--- NQueen.py
+++ NQueen.py
@@ -1,6 +1,27 @@
 import numpy as np
 import copy
 
+
+# This function calculates fitness of solution pivoting 2 columns if needed
+def fitness(queen_list, pivot):
+
+    if isinstance(pivot, int):
+        try:
+            queen_list[pivot], queen_list[pivot + 1] = queen_list[pivot + 1], queen_list[pivot]
+        except KeyError:
+            queen_list[pivot], queen_list[0] = queen_list[0], queen_list[pivot]
+
+    J = 0
+
+    for key in queen_list.keys():
+        for q in range(key + 1, len(queen_list)):
+            if abs(key - q) == abs(queen_list[key] - queen_list[q]):
+                print("SAME DIAGONAL", key, q)
+                J += 1
+
+    return J
+
+
 n = 15
 
 queens = dict()
@@ -12,16 +33,7 @@ for i in range(n):
     queens[i] = positions[np.random.randint(n - i)]
     positions.remove(queens[i])
 
-J = 0
-
-# print(queens)
-
-# Calculate fitness of solution
-for key in queens.keys():
-    for q in range(key + 1, n):
-        if abs(key - q) == abs(queens[key] - queens[q]):
-            # print("SAME DIAGONAL", key, q)
-            J += 1
+J = fitness(copy.copy(queens), None)
 
 board = np.zeros((n, n))
 
