f29cfd8b94904c240c213a5efefeba6a9011b50d
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-04-08 13:36:33 -0300

    Calculate fitness of solution and print board

--- NQueen.py
+++ NQueen.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 n = 15
 
@@ -6,9 +7,26 @@ queens = dict()
 
 positions = list(range(n))
 
-print(positions)
-
 # Create queens
 for i in range(n):
     queens[i] = positions[np.random.randint(n - i)]
     positions.remove(queens[i])
+
+J = 0
+
+# print(queens)
+
+# Calculate fitness of solution
+for key in queens.keys():
+    for q in range(key + 1, n):
+        if abs(key - q) == abs(queens[key] - queens[q]):
+            # print("SAME DIAGONAL", key, q)
+            J += 1
+
+board = np.zeros((n, n))
+
+for key in queens.keys():
+    board[key, queens[key]] = 1
+
+print(board.T)
+print(J)
